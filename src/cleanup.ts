28aaa3c8665490eb65df205f2876ad7dd593359e
Ichinose Shogo <shogo82148@gmail.com>
2020-12-29 16:54:17 +0900

    implement the action

---
+++ src/cleanup.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import * as lock from './lock'
+
+async function run() {
+  const rawState = core.getState('STATE')
+  if (!rawState) {
+    return
+  }
+  const state = JSON.parse(rawState) as lock.lockState
+  const required = {
+    required: true
+  }
+  const token = core.getInput('token', required)
+  const key = core.getInput('key', required)
+  const repository = core.getInput('repository', required)
+  const prefix = core.getInput('prefix', required)
+
+  await lock.unlock(
+    {
+      token,
+      key,
+      repository,
+      prefix
+    },
+    state
+  )
+}
+
+run()
