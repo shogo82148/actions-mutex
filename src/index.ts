cf4da80baa74726fedde3f683fa3e7d10ade1ebd
Ichinose Shogo <shogo82148@gmail.com>
2020-12-29 17:12:28 +0900

    move try and catche into async function

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,25 @@ import * as core from '@actions/core'
 import * as lock from './lock'
 
 async function run() {
-  const required = {
-    required: true
+  try {
+    const required = {
+      required: true
+    }
+    const token = core.getInput('token', required)
+    const key = core.getInput('key', required)
+    const repository = core.getInput('repository', required)
+    const prefix = core.getInput('prefix', required)
+
+    const state = await lock.lock({
+      token,
+      key,
+      repository,
+      prefix
+    })
+    core.saveState('STATE', JSON.stringify(state))
+  } catch (e) {
+    core.setFailed(e)
   }
-  const token = core.getInput('token', required)
-  const key = core.getInput('key', required)
-  const repository = core.getInput('repository', required)
-  const prefix = core.getInput('prefix', required)
-
-  const state = await lock.lock({
-    token,
-    key,
-    repository,
-    prefix
-  })
-  core.saveState('STATE', JSON.stringify(state))
 }
 
-try {
-  run()
-} catch (e) {
-  core.setFailed(e)
-}
+run()
