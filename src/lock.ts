4082df2160f3f2f2fefcc7adb758bc82abb45bc4
Ichinose Shogo <shogo82148@gmail.com>
2020-12-29 17:29:59 +0900

    the reject message might be '[remote rejected]'

--- src/lock.ts
+++ src/lock.ts
@@ -51,7 +51,7 @@ class Locker {
     if (token) {
       // configure authorize header
       const auth = Buffer.from(`x-oauth-basic:${token}`).toString('base64')
-      core.setSecret(auth)
+      core.setSecret(auth) // make sure it's secret
       await this.git('config', '--local', 'http.https://github.com/.extraheader', `AUTHORIZATION: basic ${auth}`)
     }
   }
@@ -116,7 +116,7 @@ class Locker {
     if (code == 0) {
       return true
     }
-    if (stderr.includes('[rejected]')) {
+    if (stderr.includes('[rejected]') || stderr.includes('[remote rejected]')) {
       return false
     }
     throw new Error('failed to git push: ' + code)
